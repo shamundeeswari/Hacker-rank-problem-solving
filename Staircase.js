38667f8534d4c4a8307a13730f28f0e9c09dec0d
samundeeswari <41180518+shamundeeswari@users.noreply.github.com>
2018-08-23 16:57:51 +0530

    Add files via upload

---
+++ Staircase.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the staircase function below.
+function staircase(n) {
+
+  var line = Array(n + 1).fill(' ');
+  line[n] = '';
+  for (var i = n - 1; i >= 0; i--) {
+    line[i] = '#';
+    console.log(line.join(''));
+  }
+
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    staircase(n);
+}
